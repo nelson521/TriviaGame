a084148ea77abeafa9812bd7d3326e0e33e15b6e
Stevenson Nelson <sonson521@gmail.com>
2019-06-16 18:43:48 -0400

    added javascript to page. Got click button to show game.

---
+++ assets/javascript/app.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+// Hide the game 
+$('div.container2').hide();
+// Reveal the game when clicked
+$('button').on('click', function() {
+  $('div.container2').show();
+  // Start timer once you click the button
+});
+});
