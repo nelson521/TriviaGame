cb2e071def83fb56bd5baafb76cb1c46599a45d9
Stevenson Nelson <sonson521@gmail.com>
2019-06-20 14:20:24 -0400

    fixed html and added a timer

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,9 +1,25 @@
-$(document).ready(function() {
-// Hide the game 
-$('div.container2').hide();
+$(document).ready(function () {
+  var timeStarts = 15;
+  $('#countDown').text(timeStarts);
+  var timer; 
+  // Hide the game 
+  $('div.container2').hide();
+  // function updates the timer 
+  function updateTimer() {
+    timeStarts = timeStarts - 1;
+    if(timeStarts === 0) {
+      console.log('Game Ended!');
+      clearInterval(timer);
+    }
+    $('#countDown').text(timeStarts);
+  }
 // Reveal the game when clicked
-$('button').on('click', function() {
-  $('div.container2').show();
-  // Start timer once you click the button
-});
+  $('button').on('click', function () {
+    $('div.container2').show();
+    // Start timer once you click the button
+    timer = setInterval(updateTimer, 1000);
+    // timer goes backwards starting a 15
+  });
+
+  
 });
